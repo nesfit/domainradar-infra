8a629c394f8e168056903c4251b064d09bc20d88
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2024-05-25 17:21:15 +0200

    Add an example SQL that inserts some domain names

---
+++ misc/domains_input_example.sql
@@ -0,0 +1,606 @@
+DELETE FROM "domains_input";
+
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('google.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('youtube.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('baidu.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('bilibili.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('facebook.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('instagram.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('qq.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('twitter.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('wikipedia.org', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('zhihu.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('amazon.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('bing.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('yahoo.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('reddit.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('whatsapp.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('taobao.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('xvideos.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('163.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('pornhub.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('vk.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('microsoft.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('linkedin.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('sina.com.cn', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('yandex.ru', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('tiktok.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('jd.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('weibo.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('fandom.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('t.co', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('github.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('live.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('google.com.hk', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('xhamster.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('netflix.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('yahoo.co.jp', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('aliexpress.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('spankbang.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('paypal.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('tmall.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('csdn.net', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('canva.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('naver.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('sohu.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('imdb.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('douban.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('ebay.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('mail.ru', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('xnxx.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('alipay.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('amazon.in', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('so.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('pinterest.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('hao123.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('douyu.com', now());
+
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('iqiyi.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('office.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('telegram.org', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('adobe.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('duckduckgo.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('apple.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('msn.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('spotify.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('stackoverflow.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('twitch.tv', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('microsoftonline.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('twimg.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('3dmgame.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('amazon.co.jp', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('1688.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('zoom.us', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('mega.nz', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('pixiv.net', now());
+
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('etsy.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('t.me', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('quora.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('youdao.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('realsrv.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('360.cn', now());
+
+
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('deepl.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('google.co.in', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('stripchat.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('imgur.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('doubleclick.net', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('sogou.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('chaturbate.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('avito.ru', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('hotstar.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('aliyun.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('mediafire.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('cnki.net', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('smzdm.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('nih.gov', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('bbc.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('onlyfans.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('tencent.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('youku.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('espn.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('indeed.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('redd.it', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('ok.ru', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('discord.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('jianshu.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('freepik.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('wordpress.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('soso.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('ilovepdf.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('cnn.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('booking.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('sciencedirect.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('nytimes.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('fc2.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('digikala.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('xiaohongshu.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('walmart.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('flipkart.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('douyin.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('patreon.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('instructure.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('researchgate.net', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('roblox.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('dropbox.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('chsi.com.cn', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('archive.org', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('google.de', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('medium.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('google.co.uk', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('52pojie.cn', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('rakuten.co.jp', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('tradingview.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('animeflv.net', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('alibaba.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('fiverr.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('googlevideo.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('livejasmin.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('zol.com.cn', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('dcinside.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('google.co.jp', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('ozon.ru', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('google.ru', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('speedtest.net', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('savefrom.net', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('hupu.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('y2mate.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('udemy.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('wetransfer.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('w3schools.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('eporner.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('grammarly.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('sxyprn.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('yandex.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('chase.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('ebay.co.uk', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('uol.com.br', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('tistory.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('amazonaws.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('nicovideo.jp', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('cricbuzz.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('dmm.co.jp', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('amazon.ca', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('epicgames.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('steampowered.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('binance.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('namu.wiki', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('noodlemagazine.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('bet365.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('book118.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('linktr.ee', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('espncricinfo.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('amazon.it', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('cctv.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('autohome.com.cn', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('toutiao.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('quizlet.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('deviantart.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('pinimg.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('docin.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('pconline.com.cn', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('feishu.cn', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('force.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('aliexpress.ru', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('theguardian.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('samsung.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('yts.mx', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('dailymail.co.uk', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('primevideo.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('zillow.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('ifeng.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('bbc.co.uk', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('wildberries.ru', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('hitomi.la', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('vimeo.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('missav.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('nvidia.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('shopify.com', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('nhentai.net', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('amazon.de', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('daum.net', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('amazon.co.uk', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('gofile.io', now());
+INSERT INTO "domains_input" ("domain", "added")
+    VALUES
+        ('office365.com', now());
