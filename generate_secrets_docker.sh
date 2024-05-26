b1b75c84474a9462eaad12cd437c7f6b9d8c9139
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2024-05-26 19:24:15 +0200

    Add podman support

--- generate_secrets_docker.sh
+++ generate_secrets_docker.sh
@@ -4,8 +4,23 @@
 # This script will build a docker image that runs the generate_secrets.sh script
 # to generate SSL certficates.
 
-docker build --tag domrad/generate-secrets -f dockerfiles/generate_secrets.Dockerfile \
+if docker -v; then
+  if docker -v | grep -q 'podman'; then
+    USERNS="--userns=keep-id"
+    DOCKER="podman"
+  else
+    USERNS=""
+    DOCKER="docker"
+  fi
+elif podman -v; then
+  USERNS="--userns=keep-id"
+  DOCKER="podman"
+else
+  echo "Neither docker nor podman was found."
+  exit 1
+fi
+
+$DOCKER build --tag domrad/generate-secrets -f dockerfiles/generate_secrets.Dockerfile \
     --build-arg "UID=$(id -u)" --build-arg "GID=$(id -g)" .
 mkdir -p secrets
-docker run -v "$PWD/secrets:/pipeline-all-in-one/secrets" domrad/generate-secrets:latest
-
+$DOCKER run "$USERNS" -v "$PWD/secrets:/pipeline-all-in-one/secrets" domrad/generate-secrets:latest
