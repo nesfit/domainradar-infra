bda4183c70854f1f6d6c5a13d0f2d4fb25342ecc
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2024-05-26 19:24:04 +0200

    Enable to reuse cloned files

--- build_all_images.sh
+++ build_all_images.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
+BUILD_DIR=image_build
+COLEXT_DIR="domainradar-colext"
+INPUT_DIR="domainradar-input"
+
 cd dockerfiles || exit 1
 
-mkdir -p image_build
-git clone git@github.com:nesfit/domainradar-input.git image_build/domainradar-input
-git clone git@github.com:nesfit/domainradar-colext.git image_build/domainradar-colext
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
 
+if [ -d "$INPUT_DIR" ]; then
+  echo "Pulling latest domainradar-input"
+  cd "$INPUT_DIR" || exit 1
+  git pull
+  cd ..
+else
+  git clone git@github.com:nesfit/domainradar-input.git "$INPUT_DIR"
+fi
+
+if [ -d "$COLEXT_DIR" ]; then
+  echo "Pulling latest domainradar-colext"
+  cd "$COLEXT_DIR" || exit 1
+  git pull
+  cd ..
+else
+  git clone git@github.com:nesfit/domainradar-colext.git "$COLEXT_DIR"
+fi
+
+cd ..
+
+echo "Building the prefilter image"
 docker build -f prefilter.Dockerfile -t domrad/prefilter .
-cd image_build/domainradar-colext || rm -rf image_build && exit 1
+
+echo "Building the pipeline images"
+cd "$BUILD_DIR/$COLEXT_DIR" || (rm -rf image_build && exit 1)
 ./build_docker_images.sh
 
 cd ../..
